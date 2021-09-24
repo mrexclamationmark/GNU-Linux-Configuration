02a7cff59ad1c31ec46b4f6cff53468d9e24ef32
mrtelekinesis <86800445+mrtelekinesis@users.noreply.github.com>
2021-09-24 14:23:47 +0000

    Update qdisc.sh

--- root/qdisc.sh
+++ root/qdisc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 tc qdisc del dev wlan0 root
-tc qdisc add dev wlan0 root fq_pie limit 500 flows 5 target 5ms tupdate 5ms
+#tc qdisc add dev wlan0 root fq_pie limit 500 flows 5 target 5ms tupdate 5ms
+tc qdisc add dev wlan0 root pfifo limit 500
